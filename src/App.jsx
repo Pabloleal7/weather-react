f24e59b7f76d92571b7f8b526fe7503d602aeef9
Pablo Leal <pabloleal7@hotmail.com>
2021-08-12 02:25:05 -0300

    aplicação com uma estilizacao simples e storage

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,11 @@ function App() {
 
   const [weather, setWeather] = useState()
   const [city, setCity] = useState("")
-  const [search,setSearch] = useState("")
+  const [search, setSearch] = useState("")
+  const [lista, setlista] = useState([]);
+
+
+
 
 
 
@@ -20,36 +24,58 @@ function App() {
     setCity(search)
     console.log(response.data)
     setWeather(response.data)
+
+    const minhaLista = localStorage.getItem('listacidades')
+    let cidadesSalvas = JSON.parse(minhaLista) || []
+
+    const hasCity = cidadesSalvas.some((cidadeSalva) => cidadeSalva === search.toUpperCase())
+    //alert(cidadesSalvas)
+    if (!hasCity) {
+      cidadesSalvas.push(search.toUpperCase());
+      localStorage.setItem('listacidades', JSON.stringify(cidadesSalvas));
+    }
+    
+setlista(cidadesSalvas)
+
+
+
+
+
   }
 
   async function handleGetCidadeIP() {
-    
+
     const responseCity = await apiCidadeIP.get()
     const response = await apiClima.get(responseCity.data.city)
     setCity(responseCity.data.city)
     console.log(responseCity.data.city)
     setWeather(response.data)
 
-    
-    
+
+
   }
 
 
   useEffect(() => {
     handleGetCidadeIP()
-  })
+    const minhaLista = localStorage.getItem('listacidades');
+    setlista(JSON.parse(minhaLista) || [])
+  }, [])
   return (
     <div>
       {/* <h1>{"Hello World".toUpperCase()}</h1>
    <HelloWorld/> */}
-      <header>
-        <form onSubmit={handleGetWeather} >
-          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-        </form>
-        <button onClick={handleGetWeather}>Buscar</button>
+      <header className="menu">
+        <div className="cont">
+          <form onSubmit={handleGetWeather} >
+            <input className="css-input" type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+          </form>
+          <button className="myButton" onClick={handleGetWeather}>Buscar</button>
+          <h2>Veja o clima de diversas cidades</h2>
+        </div>
       </header>
       {weather &&
-        <main>
+        <main className="conteudo">
           {/* {JSON.stringify(weather)} */}
           <h1>{city.toUpperCase()}</h1>
 
@@ -77,6 +103,23 @@ function App() {
               )}
             </ol>
 
+          </section>
+
+          <section>
+            <br /><br /><br />
+            <h1>Cidades Pesquisadas</h1>
+            <ul>
+              {lista.map((item) => {
+                return (
+
+                  <li key={item}>
+                    <span>{item}</span>
+
+                  </li>
+
+                )
+              })}
+            </ul>
           </section>
         </main>
       }
