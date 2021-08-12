b307f96ac51641a6538132ab987c65016fb0d642
Pablo Leal <pabloleal7@hotmail.com>
2021-08-11 22:51:50 -0300

    nova funcionalidade

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import HelloWorld from './components/HelloWorld'
+import { api } from "./services/api"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [weather, setWeather] = useState()
+  const city = "Itaberaba"
+
+
+  async function handleGetWeather() {
+    const response = await api.get(city)
+    console.log(response.data)
+    setWeather(response.data)
+  }
+
+
+  useEffect(()=>{
+    handleGetWeather()
+  })
   return (
-   <h1>Hellow World</h1>
+    <div>
+      {/* <h1>{"Hello World".toUpperCase()}</h1>
+   <HelloWorld/> */}
+     {/*  <header>
+        <button onClick={handleGetWeather}>Enviar</button>
+      </header> */}
+      {weather &&
+        <main>
+          {/* {JSON.stringify(weather)} */}
+          <h1>{city}</h1>
+
+          <section>
+            <h2>Clima Atual</h2>
+            <p>{weather.temperature}</p>
+            <p>{weather.description}</p>
+          </section>
+          <section>
+            <h2>Previsão</h2>
+            {weather.forecast.map(day => (
+              <li>
+                <p>{day.temperature}</p>
+                <p>{day.wind}</p>
+              </li>
+            )
+
+            )}
+
+          </section>
+        </main>
+      }
+    </div>
   )
 }
 
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function HelloWorld(){
+return(
+  <div>
+Ola Mundo
+  </div>
+)
+
+
+}
+
+//export default HelloWorld  
+
