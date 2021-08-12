dc6b3da29a58feae61a42fd9b48b8b169b4378ed
Pablo Leal <pabloleal7@hotmail.com>
2021-08-11 21:56:36 -0300

    upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>Hellow World</h1>
+  )
+}
+
+export default App
